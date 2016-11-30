e7badb6efd3dac692817bc4d687efb782f2f167d
Truong Nguyen <truongqvnguyen@kms-technology.com>
2016-11-30 11:36:38 +0700

    Restructer folder

---
+++ HomeCinema/HomeCinema/HomeCinema/Data/Configurations/GenreConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using HomeCinema.Configurations;
+
+namespace HomeCinema.Entities
+{
+    public class GenreConfiguration : EntityBaseConfiguration<Genre>
+    {
+        public GenreConfiguration()
+        {
+            Property(g => g.Name).IsRequired().HasMaxLength(50);
+        }
+    }
+}
---
+++ HomeCinema/HomeCinema/HomeCinema/Data/Configurations/RoleConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using HomeCinema.Configurations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HomeCinema.Entities
+{
+    public class RoleConfiguration : EntityBaseConfiguration<Role>
+    {
+        public RoleConfiguration()
+        {
+            Property(ur => ur.Name).IsRequired().HasMaxLength(50);
+        }
+    }
+}
